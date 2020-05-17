42c644fe15093f4046a6a9bb06b77de91e8d3b73
Deep <purohit.deep@gmail.com>
2020-05-18 00:06:17 +0200

    updated bootdisk to google optimized OS

--- compute-engine-infra/main.tf
+++ compute-engine-infra/main.tf
@@ -15,10 +15,11 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
 
